3f179c9b7b64447963e27e4ee888da09a7663fc0
Vishnu teja <vishnuteja476@gmail.com>
2026-07-14 11:25:01 +0530

    made changes to xportassessment form

--- app/(website)/xportassessmentform/page.jsx
+++ app/(website)/xportassessmentform/page.jsx
@@ -3,6 +3,8 @@
 import AppForm from "@/components/AssessmentComponets/AppForm/AppForm";
 import React from "react";
 
+import { Suspense } from "react";
+
 import { ArrowLeft, ShieldCheck, RotateCcw } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
@@ -16,18 +18,20 @@ function XportAssessment() {
   // console.log("Assessment ID:", assessmentId);
   // console.log("Token:", token);
   return (
-    <div>
-      <div className="mt-16 pt-12 bg-gray-50">
-        <div
-          onClick={() => router.back()}
-          className="inline-flex cursor-pointer ml-12 bg-black px-2 py-2 rounded-lg text-white items-center gap-2 text-sm text-gray-500  transition"
-        >
-          <ArrowLeft size={16} />
-          Back
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <div className="mt-16 pt-12 bg-gray-50">
+          <div
+            onClick={() => router.back()}
+            className="inline-flex cursor-pointer ml-12 bg-black px-2 py-2 rounded-lg text-white items-center gap-2 text-sm text-gray-500  transition"
+          >
+            <ArrowLeft size={16} />
+            Back
+          </div>
+          <AppForm />
         </div>
-        <AppForm />
       </div>
-    </div>
+    </Suspense>
   );
 }
 
