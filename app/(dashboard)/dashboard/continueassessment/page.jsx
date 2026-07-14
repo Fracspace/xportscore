34375ef2b2171ddbabe02f654495d24c06596bf1
Vishnu teja <vishnuteja476@gmail.com>
2026-07-14 09:59:03 +0530

    few more changes

--- app/(dashboard)/dashboard/continueassessment/page.jsx
+++ app/(dashboard)/dashboard/continueassessment/page.jsx
@@ -1,12 +1,15 @@
 import AppForm from "@/components/AssessmentComponets/AppForm/AppForm";
 import React from "react";
+import { Suspense } from "react";
 
 function page() {
   return (
     <div>
+         <Suspense fallback={<div>Loading...</div>}></Suspense>
       <div>
         <AppForm />
       </div>
+      <Suspense />
     </div>
   );
 }
