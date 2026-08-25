e41f540fb56977bbf93833366900e9032911313d
sai charan <utukurisaicharan123@gmail.com>
2026-08-25 17:16:34 +0530

    style: clean up commented text in Footer.jsx

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -186,9 +186,9 @@ export default function Footer() {
           <p className="text-[11px] text-gray-500 mt-3 text-center">
             © 2026 XportScore. All rights reserved.
           </p>
-          <p className="text-center text-sm font-bold text-gray-300 mt-6 tracking-wide">
+          {/* <p className="text-center text-sm font-bold text-gray-300 mt-6 tracking-wide">
             These Services are operated and maintained by FRACSPACE PRIVATE LIMITED
-          </p>
+          </p> */}
         </div>
       </div>
     </footer>
